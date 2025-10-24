6390bff81bd7a76837ab7138cf3623c2c507f55c
frgl0001 <frgl0001@stud.ek.dk>
2025-10-24 11:46:52 +0200

    Update Kunde.java
    Færdig Class, mangler ToString for pæn udskrift (Ikke nødvendigt)

--- src/Sprint1_in_progress/Kunde.java
+++ src/Sprint1_in_progress/Kunde.java
@@ -13,4 +13,30 @@ public class Kunde {
         this.kundenr = kundenr;
         this.tlf = tlf;
     }
+
+    // get+return
+    public String getNavn() {
+        return navn;
+    }
+
+    public int getKundenr() {
+        return kundenr;
+    }
+
+    public int getTlf() {
+        return tlf;
+    }
+
+    // set navn+tlf (Kundenr er en fast værdi, derfor er der ikke tilføjet en set)
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public void setTlf(int tlf) {
+        this.tlf = tlf;
+    }
+
+// ToString m. @Override skal tilføjes (Ikke nødvendigt, men gør udskriften pæn)
+
 }
