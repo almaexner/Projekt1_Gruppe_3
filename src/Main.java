b512a05fc1fa2c9aa5e4381d8a449d59d07f9363
Niko0004Stud <niko0004@stud.ek.dk>
2025-10-24 11:39:23 +0200

    Lad dette være en success

--- src/Main.java
+++ src/Main.java
@@ -12,4 +12,3 @@ public class Kunde {
         this.kundenr = kundenr;
         this.tlf = tlf;
     }
-
--- src/Sprint1_in_progress/Kalender.java
+++ src/Sprint1_in_progress/Kalender.java
@@ -1,8 +1,18 @@
 package Sprint1_in_progress;
 
-public class Kalender {
+import java.util.Date;
 
+public class Kalender {
+    // Variabler
+    Date idag;
+
+    // constructor
     public Kalender(){
-        
+        idag = new Date();
+    }
+
+    public static void main(String[] args) {
+        Kalender test = new Kalender();
+        System.out.println(test);
     }
 }
