8a6b43adfc730d9562a8acdf41c869e21959201a
Estera Turcu <estu0002@stud.ek.dk>
2025-10-24 15:16:07 +0200

    Metode til Kunde objekt

--- src/Sprint1_in_progress/Kunde.java
+++ src/Sprint1_in_progress/Kunde.java
@@ -1,6 +1,9 @@
 package Sprint1_in_progress;
 
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Kunde {
     // Variabler
     private String navn;
@@ -38,5 +41,15 @@ public class Kunde {
     }
 
 // ToString m. @Override skal tilføjes (Ikke nødvendigt, men gør udskriften pæn)
+
+//1.1.1 Metode til at gemme oprettert kunde objekt på fil
+
+    public void saveToFile(String kunde) throws IOException {
+        try (FileWriter fil = new FileWriter("kunde fil.txt")) {
+            fil.write(navn+";" + kundenr+";" + tlf);
+        System.out.println("Kunde er gemt i fil.");} catch (IOException e) {
+            System.out.println("Kunde kan ikke gemmes.");
+        }
+    }
 
 }
