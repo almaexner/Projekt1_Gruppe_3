2e801d07899fb4d991c14475fae4790e527539b5
almaexner <alje0003@stud.ek.dk>
2025-10-26 16:25:53 +0100

    Booking klasse update, Natasha&Alma

--- src/Sprint1_in_progress/Booking.java
+++ src/Sprint1_in_progress/Booking.java
@@ -2,12 +2,13 @@ package Sprint1_in_progress;
 
 public class Booking {
 
+    //Variable
     private int bookingNo;
     private String dato;
     static private int noOfBookings = 0;
 
     public Booking(String dato) {
-        noOfBookings++;
+        noOfBookings++; //Bookingnr tæller op pr. ny bookning.
         bookingNo = noOfBookings;
         this.dato = dato;
     }
