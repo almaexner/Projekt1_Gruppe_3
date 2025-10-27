3510b5739ec5e47b3a8112834ee5c97d65786a3e
Niko0004Stud <niko0004@stud.ek.dk>
2025-10-27 09:44:33 +0100

    Udkast af kalenderklasse
    Dette er kalenderklasse 1.0. med gregoriansk kalender klasse, funktionalitet til at udskrive datoer, og en array som lægges ind i.

--- src/Sprint1_in_progress/Kalender.java
+++ src/Sprint1_in_progress/Kalender.java
@@ -1,18 +1,44 @@
 package Sprint1_in_progress;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Kalender {
+    // Oprettelse af Gregoriansk kalenderobjekt
+    GregorianCalendar uge;
     // Variabler
-    Date idag;
-
+    int antalDage;
+    int day;
+    int month;
     // constructor
-    public Kalender(){
-        idag = new Date();
-    }
-
-    public static void main(String[] args) {
-        Kalender test = new Kalender();
-        System.out.println(test);
+     public Kalender(int aD){
+         uge = new GregorianCalendar();
+         month = uge.get(Calendar.MONTH);       //  Her defineres variablen for den nuværende måned plus 1,
+         month = month+1;                           //  da der ellers tælles fra nul
+         day = uge.get(Calendar.DAY_OF_MONTH);  //  Her defineres variablen for den nuværende dag i måneden
+         uge.getMaximum(Calendar.DAY_OF_MONTH);
+         int maxDays = uge.getMaximum(Calendar.DAY_OF_MONTH);   // Her defineres variablen for det maksimale
+                                                                // antal dage i netop den måned objektet bliver oprettet
+         String dato;
+         antalDage = aD;
+         String[] fremskrivning = new String[antalDage];
+         // Her bliver der udregnet og udskrevet datoer for det angivne antal dage
+         for (int i=0; i<antalDage; i++){
+             dato = day+"/"+month;
+             fremskrivning[i] = dato;
+             System.out.println("Dette er også en test: "+day+"/"+month);
+             if (day < maxDays){   // Næste dag i samme måned
+                 day = day + 1;
+             }
+             else {         // Hvis det var den sidste dag i måneden, startes der fra den første i næste måned
+                 day=1;
+                 if (month < 12){   // Efter den tolvte måned tælles der om fra den første måned
+                     month = month+1;
+                 }
+                 else {
+                     month = 1;
+                 }
+             }
+        }
     }
 }
