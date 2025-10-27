6ecacab47b8b881904003a31628a15ced7fbe9ea
almaexner <alje0003@stud.ek.dk>
2025-10-27 16:45:09 +0100

    Metode til at oprette nye bookinger //Natasha&Alma

--- src/Sprint1_in_progress/Booking.java
+++ src/Sprint1_in_progress/Booking.java
@@ -1,7 +1,10 @@
 package Sprint1_in_progress;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Booking {
 
@@ -27,6 +30,56 @@ public class Booking {
     public String toString() {
         return "Booking nr: " + bookingNo + " Dato: " + dato;
     }
+    public static Booking nyBooking() throws IOException {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Indtast ønsket dato");
+        String dato = userInput.nextLine();
+
+        Booking b = new Booking("");
+        System.out.println("Booking oprettet: " + dato);
+        b.bookingFile();
+        return b;
+    }
+    public void readFile () throws IOException{ //Metode til at læse fil igennem for at sammenligne brugerens input for tlf.nr for at se om kunden er oprettet eller ej.
+        FileReader kundeFil = new FileReader("src//kunde fil.txt"); //Her skal fil over registreret kunder indsættes
+        BufferedReader ind = new BufferedReader(kundeFil);
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Indtast telefonnummer: ");
+        String userInput=keyboard.nextLine();
+
+        boolean match = false;
+        String matchedLinje = null;
+
+        String linje =ind.readLine();
+        while (linje!=null) {
+            String[] bidder = linje.split(";");
+            String tlfFraFil = null;
+            if (bidder.length >= 2) //Tjekker at linjen i filen har 3 bidder.
+                tlfFraFil = bidder[2];
+            if (tlfFraFil.equals(userInput)) { //Sammenligner tlfnr fra kundefil med brugeren input i konsolvinduet.
+                match = true;
+                matchedLinje = linje;
+                break; //Vi stopper løkken når vi har fundet et tlf match.
+            }
+            linje=ind.readLine(); //Den stopper med at læse filen.
+        }
+
+        ind.close();
+
+        if(match) {
+            System.out.println("Kunden er oprettet");
+            Booking.nyBooking();
+            return;
+
+        } else {
+            System.out.println("Kunden er ikke oprettet");
+            Kunde k=Kunde.opretKunde();
+            k.kundeFil();
+            Booking.nyBooking();
+            return;
+        }
+    }
 
     // Metode til at gemme booking-objekt på fil
     public void bookingFile() throws IOException {
@@ -39,5 +92,9 @@ public class Booking {
             System.out.println("Booking kan ikke gemmes.");
             e.printStackTrace();
         }
+    }
+    public static void main(String[] args) throws IOException {
+        Booking test = new Booking("");
+        test.readFile();
     }
 }
