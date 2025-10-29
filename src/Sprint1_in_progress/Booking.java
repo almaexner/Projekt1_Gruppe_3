b9c142b2daabafee5f49bc571a320c63806fdaa3
frgl0001 <frgl0001@stud.ek.dk>
2025-10-29 09:39:29 +0100

    BookingUpdate
    Booking klasse opdateret så ;null; ikke opstår og exceptions/filereading fungerer. TestMain opdateret for solo test.
    
    (Udskrivning kan ændres fra "; " til andet eg.)

--- src/Sprint1_in_progress/Booking.java
+++ src/Sprint1_in_progress/Booking.java
@@ -12,11 +12,15 @@ public class Booking {
     private int bookingNo;
     private String dato;
     static private int noOfBookings = 0;
+    private static String navn;   // NY
+    private String tlf;
 
-    public Booking(String dato) {
+    public Booking(String dato, String navn, String tlf) {
         noOfBookings++; //Bookingnr tæller op pr. ny bookning.
         bookingNo = noOfBookings;
         this.dato = dato;
+        this.navn = navn;
+        this.tlf = tlf;
     }
 
     public int getBookingNo() {
@@ -30,37 +34,41 @@ public class Booking {
     public String toString() {
         return "Booking nr: " + bookingNo + " Dato: " + dato;
     }
-    public static Booking nyBooking() throws IOException {
+    public static Booking nyBooking(String navn, String tlf) throws IOException {
         Scanner userInput = new Scanner(System.in);
         System.out.println("Indtast ønsket dato");
+        new Kalender(5);
         String dato = userInput.nextLine();
-
-        Booking b = new Booking("");
-        System.out.println("Booking oprettet: " + dato);
+        //oprettelse
+        Booking b = new Booking(dato, navn, tlf);
         b.bookingFile();
+        System.out.println("Booking oprettet: " + dato);
         return b;
     }
-    public void readFile () throws IOException{ //Metode til at læse fil igennem for at sammenligne brugerens input for tlf.nr for at se om kunden er oprettet eller ej.
-        FileReader kundeFil = new FileReader("src//kunde fil.txt"); //Her skal fil over registreret kunder indsættes
+    public static void readFile () throws IOException{ //Metode til at læse fil igennem for at sammenligne brugerens input for tlf.nr for at se om kunden er oprettet eller ej.
+        FileReader kundeFil = new FileReader("kunde_fil.txt"); //Her skal fil over registreret kunder indsættes
         BufferedReader ind = new BufferedReader(kundeFil);
 
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Indtast telefonnummer: ");
         String userInput=keyboard.nextLine();
-
+        String tlf = userInput;
         boolean match = false;
-        String matchedLinje = null;
-
+        String navnFraFil = null;
         String linje =ind.readLine();
+
         while (linje!=null) {
             String[] bidder = linje.split(";");
-            String tlfFraFil = null;
-            if (bidder.length >= 3) //Tjekker at linjen i filen har 3 bidder.
-                tlfFraFil = bidder[2];
-            if (tlfFraFil != null && tlfFraFil.equals(userInput)) { //Sammenligner tlfnr fra kundefil med brugeren input i konsolvinduet.
-                match = true;
-                matchedLinje = linje;
-                break; //Vi stopper løkken når vi har fundet et tlf match.
+
+            if (bidder.length >= 3) {//Tjekker at linjen i filen har 3 bidder.
+                String navnFraLinje = bidder[0].trim();
+                String tlfFraFil = bidder[2].trim();
+                if (tlfFraFil.equals(userInput)){
+                    navnFraFil = navnFraLinje; //Sammenligner fra kundefil med brugeren input i konsolvinduet.
+                    match = true;
+                    // matchedLinje = linje;
+                    break; //Vi stopper løkken når vi har fundet et match.
+                }
             }
             linje=ind.readLine(); //Den stopper med at læse filen.
         }
@@ -69,15 +77,13 @@ public class Booking {
 
         if(match) {
             System.out.println("Kunden er oprettet");
-            Booking.nyBooking();
-            return;
+            nyBooking(navnFraFil, tlf);
 
         } else {
             System.out.println("Kunden er ikke oprettet");
-            Kunde k=Kunde.opretKunde();
-            k.kundeFil();
-            Booking.nyBooking();
-            return;
+            Kunde k = Kunde.opretKunde();
+            k.kundeFil(); //Gem kunde
+            nyBooking(k.getNavn(), String.valueOf(k.getTlf()));
         }
     }
 
@@ -85,16 +91,19 @@ public class Booking {
     public void bookingFile() throws IOException {
         try (FileWriter booking= new FileWriter("booking_fil.txt", true))
         {
-            booking.write("\n"+bookingNo+"; "+ dato);
+            booking.write(
+                    bookingNo + ";" + dato + ";" + navn + ";" + tlf + "\n");
             System.out.println("Booking er gemt.");
         }
-            catch (IOException e){
+        catch (IOException e){
             System.out.println("Booking kan ikke gemmes.");
             e.printStackTrace();
         }
     }
+
+    /*TestMain
     public static void main(String[] args) throws IOException {
-        Booking test = new Booking("");
-        test.readFile();
+        Booking.readFile();
     }
+     */
 }
